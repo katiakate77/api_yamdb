e7f67c83b945dcf5b1cd6a11ade18acd659d882a
andrei tambov <Kazakov.andrej.2001@gmail.com>
2023-03-28 21:43:06 +0300

    Add model Genre.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название жанра'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Идентификатор жанра'
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
 class Category(models.Model):
     name = models.CharField(
         max_length=256,
