a55f16f78bbaf92b4f5e6a3d0119c1ef426471d7
andrei tambov <Kazakov.andrej.2001@gmail.com>
2023-03-28 21:41:36 +0300

    Add models Title, Category.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Идентификатор категории'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
 class Title(models.Model):
     name = models.CharField(
         max_length=256,
