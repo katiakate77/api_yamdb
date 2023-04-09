ecaca5bef57af4b23ab8b77bc7aea1ba568d6cf9
katiakate77 <>
2023-04-09 11:46:52 +0300

    db parser fix

--- api_yamdb/reviews/management/commands/add_data.py
+++ api_yamdb/reviews/management/commands/add_data.py
@@ -1,4 +1,5 @@
-from csv import DictReader
+import os
+import csv
 from django.core.management.base import BaseCommand
 from reviews.models import Category, Genre, Title, GenreTitle, Comment, Review
 from users.models import User
@@ -7,58 +8,51 @@ from users.models import User
 class Command(BaseCommand):
     """
     Добавляет данные из csv файлов в модели.
-    Для запуска скрипта: python api_yamdb/manage.py add_data
+    Для запуска скрипта: python api_yamdb/manage.py add_data [модель]
     """
 
+    def add_arguments(self, parser):
+        parser.add_argument('model', type=str, help='model')
+
     def handle(self, *args, **options):
-        for row in DictReader(open('./api_yamdb/static/data/users.csv',
-                                   errors='ignore', encoding='utf-8')):
-            user = User(
-                id=row['id'], username=row['username'],
-                email=row['email'], role=row['role'],
-                bio=row['bio'], first_name=row['first_name'],
-                last_name=row['last_name']
-            )
-            user.save()
-        for row in DictReader(open('./api_yamdb/static/data/category.csv',
-                                   errors='ignore', encoding='utf-8')):
-            category = Category(
-                id=row['id'], name=row['name'], slug=row['slug']
-            )
-            category.save()
-        for row in DictReader(open('./api_yamdb/static/data/genre.csv',
-                                   errors='ignore', encoding='utf-8')):
-            genre = Genre(
-                id=row['id'], name=row['name'], slug=row['slug']
-            )
-            genre.save()
-        for row in DictReader(open('./api_yamdb/static/data/titles.csv',
-                                   errors='ignore', encoding='utf-8')):
-            title = Title(
-                id=row['id'], name=row['name'],
-                year=row['year'], category_id=row['category']
-            )
-            title.save()
-        for row in DictReader(open('./api_yamdb/static/data/review.csv',
-                                   errors='ignore', encoding='utf-8')):
-            review = Review(
-                id=row['id'], title_id=row['title_id'],
-                text=row['text'], author_id=row['author'],
-                score=row['score'], pub_date=row['pub_date']
-            )
-            review.save()
-        for row in DictReader(open('./api_yamdb/static/data/comments.csv',
-                                   errors='ignore', encoding='utf-8')):
-            comment = Comment(
-                id=row['id'], review_id=row['review_id'],
-                text=row['text'], author_id=row['author'],
-                pub_date=row['pub_date']
-            )
-            comment.save()
-        for row in DictReader(open('./api_yamdb/static/data/genre_title.csv',
-                                   errors='ignore', encoding='utf-8')):
-            genre_title = GenreTitle(
-                id=row['id'], title_id=row['title_id'],
-                genre_id=row['genre_id']
-            )
-            genre_title.save()
+        dir_path = os.path.abspath(
+            os.path.join(".", "api_yamdb", "static", "data")
+        )
+
+        path_and_model = {
+            'users': {os.path.join(dir_path, "users.csv"): User},
+            'category': {os.path.join(dir_path, "category.csv"): Category},
+            'genre': {os.path.join(dir_path, "genre.csv"): Genre},
+            'titles': {os.path.join(dir_path, "titles.csv"): Title},
+            'genre_title': {os.path.join(
+                            dir_path, "genre_title.csv"): GenreTitle},
+            'review': {os.path.join(dir_path, "review.csv"): Review},
+            'comments': {os.path.join(dir_path, "comments.csv"): Comment},
+        }
+
+        data = []
+        if options['model'].lower() in path_and_model:
+            obj = path_and_model[options['model'].lower()]
+            path = list(obj.keys())[0]
+            model = list(obj.values())[0]
+
+            with open(path, newline='', encoding='utf-8') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    data.append(row)
+                for item in data[1:]:
+                    obj_dict = {key: value for key,
+                                value in zip(data[0], item)}
+                    if options['model'].lower() == 'titles':
+                        obj_dict['category'] = Category(
+                            int(obj_dict['category'])
+                        )
+                    elif options['model'].lower() == 'review':
+                        obj_dict['author'] = User(int(obj_dict['author']))
+                    elif options['model'].lower() == 'comments':
+                        obj_dict['author'] = User(int(obj_dict['author']))
+                    obj_item = model(**obj_dict)
+                    obj_list = [obj_item]
+                    model.objects.bulk_create(obj_list)
+        else:
+            raise Exception('Такой модели нет')
